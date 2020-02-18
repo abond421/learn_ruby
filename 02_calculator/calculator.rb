17f336f5fbac85be03aae53bad05ad554e2d62cc
abond421 <alex.bond.3797@gmail.com>
2020-02-19 10:35:20 +1100

    Solutions added for 02_calculator.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
-#write your code here
+def add(num_one, num_two)
+    num_one + num_two
+end
+
+def subtract(num_one, num_two)
+    num_one - num_two
+end
+
+def sum(array)
+    array.sum
+end
+
+def multiply(*numbers)
+    total = 1
+    numbers.each { |n| total = total * n }
+    total
+end 
+
+def power(number, power)
+    number**power
+end
+
+def factorial(number)
+    total = 1
+    if number == 0 
+        return total
+    end
+    number.downto(1).reduce(:*)
+end 
