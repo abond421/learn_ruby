c31f22b887300cd26df82d8419bb9a2432cc63f7
abond421 <alex.bond.3797@gmail.com>
2020-02-19 10:01:02 +1100

    Solution added for 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,15 @@
-#write your code here
+def ftoc(temp)
+    if temp == 32
+        return 0
+    end
+    celcius = (temp - 32.0) / 1.8
+    celcius.round()
+end
+
+def ctof(temp)
+    if temp == 0
+        return 32
+    end
+    farenheight = (temp * 1.8) + 32 
+end 
+    
