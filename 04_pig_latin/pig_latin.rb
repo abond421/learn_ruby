fd8c00bb980db4002397199fbdbd5e6afe6e076d
abond421 <alex.bond.3797@gmail.com>
2020-02-19 12:09:40 +1100

    Solution for 04_pig_latin added

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,28 @@
-#write your code here
+def translate(string)  
+    string.split(' ').collect { |word| piglatin(word) }.join(" ")
+end
+
+def piglatin(string)
+    vowels = ["a", "e", "i", "o", "u"]
+
+    first_letter = string[0].downcase
+
+    if vowels.include?(first_letter)
+        "#{string}ay"
+    elsif string[0..1] == "qu"
+        string[2..-1] + "quay"
+    elsif string[1..2] == "qu"
+        string[3..-1] + string[0..2] + "ay"
+    else
+        consonants = []
+        consonants << string[0]        
+        if vowels.include?(string[1]) == false
+            consonants << string[1]          
+        if vowels.include?(string[2]) == false
+              consonants << string[2]          
+            end
+        end
+        "#{string[consonants.length..-1] + consonants.join + "ay"}"
+    end
+end 
+    
