f63660369b555a84afe0f866898594c6daf25ba3
abond421 <alex.bond.3797@gmail.com>
2020-02-19 11:13:42 +1100

    Solutions for 03_simon_says added

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
-#write your code here
+
+def echo(string) 
+    string
+end
+    
+def shout(string)
+    string.upcase
+end
+
+def repeat(string, number = 2)
+    ([string] * number).join(" ")
+end
+
+def start_of_word(string, length)
+    string[0..length-1]
+end
+
+def first_word(string)
+    string.split[0]
+end 
+
+def titleize(string)
+    string.capitalize!
+    small_words = ["and", "of", "the", "over"]
+    formatted_string = string.split(" ").map {|word|
+        if small_words.include?(word)
+            word
+        else
+            word.capitalize
+        end
+    }.join(" ")
+    formatted_string
+end 
