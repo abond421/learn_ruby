fdfd30de2d2ddd9a91cbaf7cafe7fa417f7ef9ba
abond421 <alex.bond.3797@gmail.com>
2020-02-20 15:05:38 +1100

    Solution added for 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+    attr_accessor :title
+
+    def title=(new_title)
+        new_title.capitalize!
+        small_words = ["an", "a", "and", "in", "of", "the"]
+        formatted_string = new_title.split(" ").map {|word|
+            if small_words.include?(word)
+                word
+            else 
+                word.capitalize
+            end 
+        }.join(" ")
+        @title = formatted_string
+    end
 end
